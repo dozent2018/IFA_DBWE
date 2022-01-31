3cb144b61d113b890470f6bc51c5338ada9cd1cc
dozent2018 <40352006+dozent2018@users.noreply.github.com>
2022-01-31 13:30:58 +0100

    Update create_beispiele.sql

--- SQL/create_beispiele.sql
+++ SQL/create_beispiele.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `adresse`
 --
 
+CREATE DATABASE IF NOT EXISTS beispiele;
+USE beispiele;
+
 DROP TABLE IF EXISTS `adresse`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
