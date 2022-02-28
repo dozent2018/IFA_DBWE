992f4980fb03d6f8ca25bff746d58b63a7e54714
Jochen Reinholdt <jochen.reinholdt@syde-consulting.ch>
2022-02-28 20:47:35 +0100

    routes.py mit request-Objekt

--- Flask/kap-2/app/routes.py
+++ Flask/kap-2/app/routes.py
@@ -4,3 +4,9 @@ from app import app
 @app.route('/index')
 def index():
     return "Hello, Flask!"
+
+from flask import request
+@app.route('/useragent')
+def uagent():
+  user_agent = request.headers.get('User-Agent')
+  return '<p> Ihr Browser: {}</p>'.format(user_agent)
